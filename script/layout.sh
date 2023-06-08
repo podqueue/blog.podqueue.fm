936e7552e6dceb5bc140e155d17c59f4a8b352fd
Ryan Baumann <ryan.baumann@gmail.com>
2023-06-08 11:19:41 -0400

    Make script more robust

--- script/layout.sh
+++ script/layout.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 pushd _layouts
-wget 'https://podqueue.fm/welcome/layout' && gsed -i -e '/csrf-token/d' layout && cp layout layout.orig && patch -F 3 layout layout.patch && mv -v layout default.html
-diff -u layout.orig default.html > layout.patch
+wget 'https://podqueue.fm/welcome/layout' && gsed -i -e '/csrf-token/d' layout && cp layout layout.orig && patch -F 3 layout layout.patch && mv -v layout default.html && diff -u layout.orig default.html > layout.patch
 rm -f layout.orig
 popd
