0a60eb684c573c61509245cb43306153fb6f2fe6
Ryan Baumann <ryan.baumann@gmail.com>
2023-07-11 11:09:11 -0400

    Remove timestamp from diff patch header

--- script/layout.sh
+++ script/layout.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 pushd _layouts
-wget 'https://podqueue.fm/welcome/layout' && gsed -i -e '/csrf-token/d' -e '/assets\.podqueue\.fm\/assets\/honeybadger-/d' layout && cp layout layout.orig && patch -F 3 layout layout.patch && mv -v layout default.html && diff -u layout.orig default.html > layout.patch
+wget 'https://podqueue.fm/welcome/layout' && gsed -i -e '/csrf-token/d' -e '/assets\.podqueue\.fm\/assets\/honeybadger-/d' layout && cp layout layout.orig && patch -F 3 layout layout.patch && mv -v layout default.html && diff -u -L layout.org -L default.html layout.orig default.html > layout.patch
 rm -f layout.orig
 popd
